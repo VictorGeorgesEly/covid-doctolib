d3ebeaeec5a7e238f9fd85cf9c83dd4bdfc925d0
Victor ELY <victor.georges.ely@gmail.com>
2021-05-07 16:55:00 +0200

    Adding the date in the url

--- covid.sh
+++ covid.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-crozon=$(curl -s https://partners.doctolib.fr/availabilities.json\?start_date\=2021-05-07\&visit_motive_ids\=2737067\&agenda_ids\=448538-448537-461784-411464-466406-411458-411465-452427-453263-464346\&insurance_sector\=public\&practice_ids\=164946\&destroy_temporary\=true\&limit\=7)
-chateaulin=$(curl -s https://partners.doctolib.fr/availabilities.json\?start_date\=2021-05-07\&visit_motive_ids\=2737067\&agenda_ids\=448541-461752-411462-411460-448542-410749-452425-453262-464345-466508\&insurance_sector\=public\&practice_ids\=164629\&destroy_temporary\=true\&limit\=7)
+startUrlCrozon=https://partners.doctolib.fr/availabilities.json\?start_date\=
+endUrlCrozon=\&visit_motive_ids\=2737067\&agenda_ids\=448538-448537-461784-411464-466406-411458-411465-452427-453263-464346\&insurance_sector\=public\&practice_ids\=164946\&destroy_temporary\=true\&limit\=7
+startUrlChateaulin=https://partners.doctolib.fr/availabilities.json\?start_date\=
+endUrlChateaulin=\&visit_motive_ids\=2737067\&agenda_ids\=448541-461752-411462-411460-448542-410749-452425-453262-464345-466508\&insurance_sector\=public\&practice_ids\=164629\&destroy_temporary\=true\&limit\=7
+date=$(date +"%Y-%m-%m")
+crozon=$(curl -s $startUrlCrozon$date$endUrlCrozon)
+chateaulin=$(curl -s $startUrlChateaulin$date$endUrlChateaulin)
 counter=0
 
 echo $crozon
@@ -10,8 +15,9 @@ echo $counter
 
 while [[ $crozon == *"Aucune disponibilité en ligne."* ]] || [[ $chateaulin == *"Aucune disponibilité en ligne."* ]]
 do
-    crozon=$(curl -s https://partners.doctolib.fr/availabilities.json\?start_date\=2021-05-07\&visit_motive_ids\=2737067\&agenda_ids\=448538-448537-461784-411464-466406-411458-411465-452427-453263-464346\&insurance_sector\=public\&practice_ids\=164946\&destroy_temporary\=true\&limit\=7)
-    chateaulin=$(curl -s https://partners.doctolib.fr/availabilities.json\?start_date\=2021-05-07\&visit_motive_ids\=2737067\&agenda_ids\=448541-461752-411462-411460-448542-410749-452425-453262-464345-466508\&insurance_sector\=public\&practice_ids\=164629\&destroy_temporary\=true\&limit\=7)
+    date=$(date +"%Y-%m-%m")
+    crozon=$(curl -s $startUrlCrozon$date$endUrlCrozon)
+    chateaulin=$(curl -s $startUrlChateaulin$date$endUrlChateaulin)
     if [[ $crozon != *"Aucune disponibilité en ligne."* ]]; then
       echo "y'a du vaccin sur Crozon !!"
     fi
