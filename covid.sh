19f84d9966a0cd0332ffe44bfb791951c2e5a01c
Victor ELY <victor.georges.ely@gmail.com>
2021-05-08 12:54:13 +0200

    Launch Google Chrome

--- covid.sh
+++ covid.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-startUrlCrozon=https://partners.doctolib.fr/availabilities.json\?start_date\=
-endUrlCrozon=\&visit_motive_ids\=2737067\&agenda_ids\=448538-448537-461784-411464-466406-411458-411465-452427-453263-464346\&insurance_sector\=public\&practice_ids\=164946\&destroy_temporary\=true\&limit\=7
-startUrlChateaulin=https://partners.doctolib.fr/availabilities.json\?start_date\=
-endUrlChateaulin=\&visit_motive_ids\=2737067\&agenda_ids\=448541-461752-411462-411460-448542-410749-452425-453262-464345-466508\&insurance_sector\=public\&practice_ids\=164629\&destroy_temporary\=true\&limit\=7
+startUrlCrozon=https://partners.doctolib.fr/availabilities.json\?start_date'='
+endUrlCrozon=\&visit_motive_ids'='2737067\&agenda_ids'='448538-448537-461784-411464-466406-411458-411465-452427-453263-464346\&insurance_sector'='public\&practice_ids'='164946\&destroy_temporary'='true\&limit'='7
+startUrlChateaulin=https://partners.doctolib.fr/availabilities.json\?start_date'='
+endUrlChateaulin=\&visit_motive_ids'='2737067\&agenda_ids'='448541-461752-411462-411460-448542-410749-452425-453262-464345-466508\&insurance_sector'='public\&practice_ids'='164629\&destroy_temporary'='true\&limit'='7
 date=$(date +"%Y-%m-%m")
 crozon=$(curl -s "$startUrlCrozon""$date""$endUrlCrozon")
 chateaulin=$(curl -s "$startUrlChateaulin""$date""$endUrlChateaulin")
@@ -18,13 +18,19 @@ do
     date=$(date +"%Y-%m-%m")
     crozon=$(curl -s "$startUrlCrozon""$date""$endUrlCrozon")
     chateaulin=$(curl -s "$startUrlChateaulin""$date""$endUrlChateaulin")
+
     if [[ $crozon != *"Aucune disponibilité en ligne."* ]]; then
-      echo "y'a du vaccin sur Crozon !!"
+      echo -e "\e[31my'a du vaccin sur Crozon !!\e[0m"
+      echo -e "https://www.doctolib.fr/centre-de-sante/chateaulin/centre-de-vaccination-covid-19-chateaulin-crozon?pid=practice-164946"
+      open -a "Google Chrome" https://www.doctolib.fr/centre-de-sante/chateaulin/centre-de-vaccination-covid-19-chateaulin-crozon?pid=practice-164946
     fi
 
     if [[ $chateaulin != *"Aucune disponibilité en ligne."* ]]; then
-        echo "y'a du vaccin sur Chateaulin !!"
+        echo -e "\e[31my'a du vaccin sur Chateaulin !!\e[0m"
+        echo -e "https://www.doctolib.fr/centre-de-sante/chateaulin/centre-de-vaccination-covid-19-chateaulin-crozon?pid=practice-164629"
+        open -a "Google Chrome" https://www.doctolib.fr/centre-de-sante/chateaulin/centre-de-vaccination-covid-19-chateaulin-crozon?pid=practice-164629
     fi
+
     sleep $(( ( RANDOM % 5 )  + 1 ))
     (( counter++ )) || true
     echo "$counter"
